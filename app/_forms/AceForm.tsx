ecbc344d8cc4c37fbada37d936161d06a012d6e5
marius-mather <marius.mather@sydney.edu.au>
2024-07-15 09:15:16 +1000

    Rework form to use FormProvider

--- app/_forms/AceForm.tsx
+++ app/_forms/AceForm.tsx
@@ -1,24 +1,13 @@
 "use client";
 import {
-  FieldError,
-  FieldName,
-  FormState,
   SubmitHandler,
   useForm,
   useWatch,
+  FormProvider,
 } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { AceScaleScores, AceScaleScoresSchema } from "@/app/_forms/schemas/ace";
-import {
-  Button,
-  FloatingLabel,
-  FloatingLabelColor,
-  FlowbiteColors,
-  FlowbiteStateColors,
-  FlowbiteTextInputColors,
-  Label,
-  TextInput,
-} from "flowbite-react";
+import { Button } from "flowbite-react";
 import { DisplayTotal } from "@/app/_forms/components/DisplayTotal";
 import ScorePlot from "@/app/_forms/components/ScorePlot";
 import ScoreInput from "@/app/_forms/components/ScoreInput";
@@ -36,24 +25,26 @@ export default function AceForm() {
   const current_value = useWatch({ control });
   const onSubmit: SubmitHandler<AceScaleScores> = (data) => console.log(data);
   return (
-    <div id="ace-form" className="flex-col">
-      <form
-        className="flex flex-wrap max-w-lg gap-4 my-2"
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <ScoreInput label="Attention" name="attention" form_return={form} />
-        <ScoreInput label="Memory" name="memory" form_return={form} />
-        <ScoreInput label="Fluency" name="fluency" form_return={form} />
-        <ScoreInput label="Language" name="language" form_return={form} />
-        <ScoreInput label="Visuospatial" name="visual" form_return={form} />
-      </form>
-      <Button type="submit" className="my-2">
-        Submit
-      </Button>
-      <div id="data-display" className="flex-col max-w-lg gap-4">
-        <DisplayTotal scores={current_value} />
-        <ScorePlot scores={current_value} />
+    <FormProvider {...form}>
+      <div id="ace-form" className="flex-col">
+        <form
+          className="flex flex-wrap max-w-lg gap-4 my-2"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <ScoreInput label="Attention" name="attention" form_return={form} />
+          <ScoreInput label="Memory" name="memory" form_return={form} />
+          <ScoreInput label="Fluency" name="fluency" form_return={form} />
+          <ScoreInput label="Language" name="language" form_return={form} />
+          <ScoreInput label="Visuospatial" name="visual" form_return={form} />
+        </form>
+        <Button type="submit" className="my-2">
+          Submit
+        </Button>
+        <div id="data-display" className="flex-col max-w-lg gap-4">
+          <DisplayTotal scores={current_value} />
+          <ScorePlot scores={current_value} />
+        </div>
       </div>
-    </div>
+    </FormProvider>
   );
 }
