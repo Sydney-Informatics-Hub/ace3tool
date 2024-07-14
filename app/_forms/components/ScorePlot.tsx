51ef72d4a0ccb9aaebb0d71854f9cf2ac36122f6
marius-mather <marius.mather@sydney.edu.au>
2024-07-15 09:02:36 +1000

    Tweak score plot

--- app/_forms/components/ScorePlot.tsx
+++ app/_forms/components/ScorePlot.tsx
@@ -5,9 +5,9 @@ import {
   Bar,
   BarChart,
   CartesianGrid,
-  CartesianAxis,
   XAxis,
   Cell,
+  YAxis,
 } from "recharts";
 import { AceScaleScores, AceScales } from "@/app/_forms/schemas/ace";
 import { schemeTableau10 } from "d3-scale-chromatic";
@@ -27,6 +27,7 @@ export default function ScorePlot(props: ScorePlotProps) {
       <BarChart data={data} width={100} height={100}>
         <CartesianGrid strokeDasharray="1" />
         <XAxis name="Scale" dataKey="name" />
+        <YAxis name="Y" dataKey="y" domain={[0, 30]} />
         <Bar dataKey="y">
           {data.map((entry, index) => {
             return <Cell fill={schemeTableau10[index]} key={index} />;
