f4fd162398b453fad505713828f7fa2950d49042
marius-mather <marius.mather@sydney.edu.au>
2024-07-15 09:00:37 +1000

    Add validation to score input

--- app/_forms/components/ScoreInput.tsx
+++ app/_forms/components/ScoreInput.tsx
@@ -2,23 +2,27 @@ import { ComponentProps } from "react";
 import { Label, TextInput } from "flowbite-react";
 import { UseFormReturn } from "react-hook-form";
 
-interface InputProps extends ComponentProps<typeof TextInput> {
+interface ScoreInputProps extends ComponentProps<typeof TextInput> {
   label: string;
   name: string;
   form_return: UseFormReturn;
 }
 
-export default function Input(props: InputProps) {
+export default function ScoreInput(props: ScoreInputProps) {
   const { label, name, form_return, ...input_props } = props;
+  const { register, getFieldState } = form_return;
+  const state = getFieldState(name);
+  const colour = state.isDirty
+    ? state.invalid
+      ? "failure"
+      : "success"
+    : undefined;
 
   return (
-    <>
+    <div>
       <Label htmlFor={name}>{label}</Label>
-      <TextInput
-        type="number"
-        color={attention_valid}
-        {...register("attention")}
-      />
-    </>
+      <TextInput type="number" color={colour} {...register(name)} />
+      {state.error && state.error.message}
+    </div>
   );
 }
