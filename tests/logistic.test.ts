e7646c2e78f84dbe1bd10be1409e82bb9dc1631f
marius-mather <marius.mather@sydney.edu.au>
2024-07-18 15:58:24 +1000

    Update tests for logistic model

--- tests/logistic.test.ts
+++ tests/logistic.test.ts
@@ -1,12 +1,6 @@
 import { create, all } from "mathjs";
 import { describe, test, expect } from "vitest";
-import {
-  Model,
-  Data,
-  predict,
-  confidence_interval,
-  qnorm,
-} from "@/lib/logistic";
+import { Data, qnorm, LogisticModel } from "@/lib/logistic";
 
 const math = create(all);
 
@@ -17,54 +11,51 @@ test("qnorm returns expected values", () => {
 describe("Logistic regression", () => {
   test("We can do a basic probability prediction", () => {
     // Example from wikipedia: https://en.wikipedia.org/wiki/Logistic_regression#Predictions
-    const model: Model = {
-      coefs: {
-        intercept: -4.1,
-        coefs: { study_hours: 1.5 },
-      },
-    };
+    const model = new LogisticModel({
+      intercept: -4.1,
+      coefs: { study_hours: 1.5 },
+    });
     const data1: Data = { study_hours: 2 };
-    const prob1 = predict(model, data1);
+    const prob1 = model.predict(data1);
     // Wikipedia only gives these to 2 decimal points
     expect(prob1).toBeCloseTo(0.25, 2);
 
     const data2: Data = { study_hours: 4 };
-    const prob2 = predict(model, data2);
+    const prob2 = model.predict(data2);
     expect(prob2).toBeCloseTo(0.87, 2);
   });
 
   // Currently getting these results/examples from a standalone python script,
   //   explore how to generate examples more easily
   test("More involved example with multiple predictors", () => {
-    const model: Model = {
+    const model = new LogisticModel({
+      intercept: -3.4495483976684698,
       coefs: {
-        intercept: -3.4495483976684698,
-        coefs: {
-          gre: 0.002293959504444253,
-          gpa: 0.7770135737198534,
-          rank: -0.5600313868499884,
-        },
+        gre: 0.002293959504444253,
+        gpa: 0.7770135737198534,
+        rank: -0.5600313868499884,
       },
-    };
+    });
 
     const data1: Data = { gre: 800.0, gpa: 4.0, rank: 1.0 };
     const expected_prob1 = 0.7178136069045331;
-    const prob1 = predict(model, data1);
+    const prob1 = model.predict(data1);
     // TODO: work out what level of precision we need and what we can achieve
     expect(prob1).toBeCloseTo(expected_prob1, 7);
   });
 
+  test("Prediction interval throws error when vcov isn't provided", () => {
+    const model = new LogisticModel({
+      intercept: -4.1,
+      coefs: { study_hours: 1.5 },
+    });
+    const data1: Data = { study_hours: 2 };
+    expect(() => model.confidence_interval(data1)).toThrowError();
+  });
+
   test("We can predict with prediction intervals", () => {
-    const model: Model = {
-      coefs: {
-        intercept: -3.4495483976684698,
-        coefs: {
-          gre: 0.002293959504444253,
-          gpa: 0.7770135737198534,
-          rank: -0.5600313868499884,
-        },
-      },
-    };
+    // These values found by running a simple logistic regression
+    //   model in Python
     const vcov = math.matrix([
       [
         1.2833400790865244, -0.00031537768873402824, -0.29029115578663295,
@@ -83,13 +74,24 @@ describe("Logistic regression", () => {
         0.01616381401554067,
       ],
     ]);
+    const model = new LogisticModel(
+      {
+        intercept: -3.4495483976684698,
+        coefs: {
+          gre: 0.002293959504444253,
+          gpa: 0.7770135737198534,
+          rank: -0.5600313868499884,
+        },
+      },
+      { vcov }
+    );
     const data1: Data = { gre: 800.0, gpa: 4.0, rank: 1.0 };
     const expected_ci = [0.58409564, 0.82166586];
     const expected_ci50 = [0.67463308, 0.75732489];
-    const se = confidence_interval(model, vcov, data1);
+    const se = model.confidence_interval(data1);
     expect(se[0]).toBeCloseTo(expected_ci[0], 5);
     expect(se[1]).toBeCloseTo(expected_ci[1], 5);
-    const ci50 = confidence_interval(model, vcov, data1, 0.5);
+    const ci50 = model.confidence_interval(data1, 0.5);
     expect(ci50[0]).toBeCloseTo(expected_ci50[0], 5);
     expect(ci50[1]).toBeCloseTo(expected_ci50[1], 5);
   });
