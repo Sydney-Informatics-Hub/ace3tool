572ac9706f2e47a2e7a823c6b99d96e280265e6a
marius-mather <marius.mather@sydney.edu.au>
2024-07-19 16:25:15 +1000

    Improved error messages/labels for score schema

--- app/_forms/schemas/ace.ts
+++ app/_forms/schemas/ace.ts
@@ -1,12 +1,32 @@
-import { object, number, InferType } from "yup";
+import { object, number, InferType, Message } from "yup";
+
+const max_score_message: Message = (data) => {
+  return `The score for ${data.label} should be less than ${data.max}`;
+};
 
 // TODO: Should these all be required?
 export const AceScaleScoresSchema = object({
-  attention: number().integer().min(0).max(18),
-  memory: number().integer().min(0).max(26),
-  fluency: number().integer().min(0).max(14),
-  language: number().integer().min(0).max(26),
-  visual: number().integer().min(0).max(16),
+  attention: number()
+    .label("Attention")
+    .integer()
+    .min(0)
+    .max(18, max_score_message),
+  memory: number().label("Memory").integer().min(0).max(26, max_score_message),
+  fluency: number()
+    .label("Fluency")
+    .integer()
+    .min(0)
+    .max(14, max_score_message),
+  language: number()
+    .label("Language")
+    .integer()
+    .min(0)
+    .max(26, max_score_message),
+  visual: number()
+    .label("Visuospatial")
+    .integer()
+    .min(0)
+    .max(16, max_score_message),
 });
 
 export type AceScaleScores = InferType<typeof AceScaleScoresSchema>;
