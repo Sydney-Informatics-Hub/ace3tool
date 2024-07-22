8183a1c82367c1bf6ea34d7190a5a3927a5e32bb
marius-mather <marius.mather@sydney.edu.au>
2024-07-22 15:31:38 +1000

    Risk plot to show predicted risk

---
+++ app/_plots/RiskPlot.tsx
@@ -0,0 +1,68 @@
+"use client";
+import React, { useEffect, useRef } from "react";
+import * as Plot from "@observablehq/plot";
+import { AceScaleScores } from "@/app/_forms/schemas/ace";
+import { LogisticModel } from "@/lib/logistic";
+import { useValidatedScores } from "@/app/_hooks/useValidatedScores";
+
+function create_risk_ranges(n: number) {
+  let start = 0;
+  const width = 1 / n;
+  const ranges: { start: number; end: number }[] = [];
+  [...Array(n).keys()].map((key) => {
+    ranges.push({ start, end: start + width });
+    start += width;
+  });
+  return ranges;
+}
+
+const ranges = create_risk_ranges(20);
+
+interface RiskPlotProps {
+  scores: Partial<AceScaleScores>;
+  model: LogisticModel<keyof AceScaleScores>;
+}
+
+export default function RiskPlot(props: RiskPlotProps) {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const { model } = props;
+  const { valid, scores } = useValidatedScores(props.scores);
+  const risk = scores ? model.predict(scores) : undefined;
+
+  useEffect(() => {
+    const plot = Plot.plot({
+      title: "Predicted risk",
+      width: 500,
+      height: 150,
+      x: { grid: true, label: "Risk" },
+      color: {
+        type: "sequential",
+        scheme: "magma",
+        domain: [0.0, 1.0],
+      },
+      marks: [
+        Plot.axisY({ ticks: [] }),
+        Plot.rect(ranges, {
+          x1: "start",
+          x2: "end",
+          y1: 0,
+          y2: 10,
+          fill: "start",
+          opacity: 0.7,
+        }),
+        // Only show total marker once we have a valid total
+        risk
+          ? Plot.tickX([{ score: risk }], {
+              x: "score",
+              stroke: "black",
+              marker: "circle",
+              strokeWidth: 2,
+            })
+          : null,
+      ],
+    });
+    containerRef?.current?.append(plot);
+    return () => plot.remove();
+  });
+  return <div ref={containerRef} />;
+}
