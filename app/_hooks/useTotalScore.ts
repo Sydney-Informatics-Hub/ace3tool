c306b4089b522176d767788ddb9c0896a7c96043
marius-mather <marius.mather@sydney.edu.au>
2024-07-22 13:14:35 +1000

    Create a hook to get the total score

---
+++ app/_hooks/useTotalScore.ts
@@ -0,0 +1,25 @@
+import { AceScaleScores, AceScaleScoresSchema } from "@/app/_forms/schemas/ace";
+import { useEffect, useState } from "react";
+
+export function useTotalScore(scores: AceScaleScores) {
+  const [valid, setValid] = useState(false);
+  const [total, setTotal] = useState<number>(0);
+
+  useEffect(() => {
+    AceScaleScoresSchema.validate(scores)
+      .then((parsed) => {
+        const sum = Object.values(parsed).reduce(
+          (sum, score) => sum + score,
+          0
+        );
+        setValid(true);
+        setTotal(sum);
+      })
+      .catch((err) => {
+        setValid(false);
+        return;
+      });
+  }, [scores]);
+
+  return { valid, total };
+}
