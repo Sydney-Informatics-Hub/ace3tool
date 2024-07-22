2a8b9ebc71b55361fd55a846ff78ffd8351bf1fa
marius-mather <marius.mather@sydney.edu.au>
2024-07-22 15:25:32 +1000

    Add means to JSON data (only approximate for now)

--- export_logistic_model.R
+++ export_logistic_model.R
@@ -24,10 +24,20 @@ vcov_matrix <- vcov(model)
 vcov_matrix %>%
     toJSON(digits = 20)
 
+# TODO: Need the actual data means here
+means <- model$data[,2:6] %>% summarise_all(min) %>% mutate_all(~ .x * -1)
+
+means_export <- means %>%
+    as.list() %>%
+    set_names(~ .x %>%
+                  str_remove("ACEIII..Subtotal") %>%
+                  str_to_lower())
+
 
 model_export <- list(
     coefs = coef_export,
-    vcov = vcov_matrix
+    vcov = vcov_matrix,
+    means = means_export
 )
 
 model_export %>%
