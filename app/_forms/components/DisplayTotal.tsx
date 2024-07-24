b13fb83bdf2222e8175e2dece0c0e4c80beaa5fe
marius-mather <marius.mather@sydney.edu.au>
2024-07-24 16:37:54 +1000

    Add predicted risk to total display

--- app/_forms/components/DisplayTotal.tsx
+++ app/_forms/components/DisplayTotal.tsx
@@ -2,17 +2,33 @@
 import { AceScaleScores } from "@/app/_forms/schemas/ace";
 import { Badge } from "flowbite-react";
 import { useTotalScore } from "@/app/_hooks/useTotalScore";
+import { LogisticModel } from "@/lib/logistic";
+import { useValidatedScores } from "@/app/_hooks/useValidatedScores";
 
 interface DisplayTotalProps {
   scores: Partial<AceScaleScores>;
+  model: LogisticModel<keyof AceScaleScores>;
 }
+
 export function DisplayTotal(props: DisplayTotalProps) {
-  const { scores } = props;
+  const { scores, model } = props;
   const { valid, total } = useTotalScore(scores);
+  const { scores: validated_scores } = useValidatedScores(scores);
+  const risk = validated_scores
+    ? 1 - model.predict(validated_scores)
+    : undefined;
+  if (risk) {
+    console.log(model.get_centered_data(validated_scores!));
+  }
 
   return (
-    <Badge className="max-w-sm justify-center" size="lg">
-      {valid ? total : "..."}
-    </Badge>
+    <>
+      <Badge className="max-w-sm justify-center" size="lg">
+        Total score: {valid ? total : "..."}
+      </Badge>
+      <Badge className="max-w-sm justify-center" size="lg">
+        Predicted risk: {risk ? risk.toPrecision(3) : "..."}
+      </Badge>
+    </>
   );
 }
