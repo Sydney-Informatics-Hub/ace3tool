757937b8124f6936608c43a660af637df8d072d4
marius-mather <marius.mather@sydney.edu.au>
2024-07-25 15:42:43 +1000

    Reduce padding on ScorePlot card

--- app/_components/ScoreWidget.tsx
+++ app/_components/ScoreWidget.tsx
@@ -6,9 +6,28 @@ import { DisplayTotal } from "@/app/_forms/components/DisplayTotal";
 import ScorePlot from "@/app/_plots/ScorePlot";
 import TotalScorePlot from "@/app/_plots/TotalScorePlot";
 import AceForm from "@/app/_forms/AceForm";
-import { Card } from "flowbite-react";
+import { Card, FlowbiteCardTheme } from "flowbite-react";
 import RiskPlot from "@/app/_plots/RiskPlot";
 import model from "@/app/_model/model";
+
+const NoPaddingCardTheme: FlowbiteCardTheme = {
+  root: {
+    base: "flex rounded-lg border border-gray-200 bg-white shadow-md dark:border-gray-700 dark:bg-gray-800",
+    children: "flex h-full flex-col justify-center gap-4 p-2 md:p-6",
+    horizontal: {
+      off: "flex-col",
+      on: "flex-col md:max-w-xl md:flex-row",
+    },
+    href: "hover:bg-gray-100 dark:hover:bg-gray-700",
+  },
+  img: {
+    base: "",
+    horizontal: {
+      off: "rounded-t-lg",
+      on: "h-96 w-full rounded-t-lg object-cover md:h-auto md:w-48 md:rounded-none md:rounded-l-lg",
+    },
+  },
+};
 
 export default function ScoreWidget() {
   const form = useForm<AceScaleScores>({
@@ -24,12 +43,13 @@ export default function ScoreWidget() {
   return (
     <div
       id="ace-form"
-      className="flex flex-col lg:flex-row lg:flex-wrap space-x-4 space-y-4 "
+      className="flex flex-col lg:flex-row lg:flex-wrap md:space-x-4 space-y-4 "
     >
       <AceForm form={form} />
       <Card
         id="data-display-simple"
         className="flex-col max-w-xl space-y-4 min-w-96"
+        theme={NoPaddingCardTheme}
       >
         <DisplayTotal scores={current_value} model={model} />
         <ScorePlot scores={current_value} />
