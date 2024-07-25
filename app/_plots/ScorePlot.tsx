28a63a96f8283276efeb34f738b034c5f6ba6327
marius-mather <marius.mather@sydney.edu.au>
2024-07-25 15:43:08 +1000

    Try to improve scoreplot appearance

--- app/_plots/ScorePlot.tsx
+++ app/_plots/ScorePlot.tsx
@@ -8,6 +8,8 @@ import {
   XAxis,
   Cell,
   YAxis,
+  XAxisProps,
+  LabelProps,
 } from "recharts";
 import { AceScaleScores, AceScales } from "@/app/_forms/schemas/ace";
 import { schemeTableau10 } from "d3-scale-chromatic";
@@ -16,6 +18,26 @@ interface ScorePlotProps {
   scores: Partial<AceScaleScores>;
 }
 
+const CustomizedAxisTick = (props: any) => {
+  const { x, y, stroke, payload } = props;
+
+  return (
+    <g transform={`translate(${x},${y})`}>
+      <text
+        x={0}
+        y={0}
+        dy={16}
+        dx={16}
+        textAnchor="end"
+        fill="#666"
+        transform="rotate(-35)"
+      >
+        {payload.value}
+      </text>
+    </g>
+  );
+};
+
 export default function ScorePlot(props: ScorePlotProps) {
   const { scores } = props;
   const data = AceScales.map((scale) => {
@@ -23,10 +45,20 @@ export default function ScorePlot(props: ScorePlotProps) {
   });
 
   return (
-    <ResponsiveContainer width={500} height={400}>
-      <BarChart data={data} width={100} height={100}>
+    <ResponsiveContainer width="100%" height={500} minWidth={300}>
+      <BarChart
+        data={data}
+        width={100}
+        height={100}
+        margin={{ top: 0, right: 20, bottom: 0, left: 0 }}
+      >
         <CartesianGrid strokeDasharray="1" />
-        <XAxis name="Scale" dataKey="name" />
+        <XAxis
+          name="Scale"
+          dataKey="name"
+          tick={<CustomizedAxisTick />}
+          height={50}
+        />
         <YAxis name="Y" dataKey="y" domain={[0, 30]} />
         <Bar dataKey="y">
           {data.map((entry, index) => {
