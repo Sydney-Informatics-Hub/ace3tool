27a795b93c0def433f9cbfd6a9aa528c3fd3c911
marius-mather <marius.mather@sydney.edu.au>
2024-07-25 15:43:42 +1000

    Add page metadata for home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
 import ScoreWidget from "@/app/_components/ScoreWidget";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "ACE-III risk calculator",
+  description: "Predict risk of impairment based on ACE-III scores",
+};
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen items-center justify-center gap-2 dark:bg-gray-800">
+    <main className="flex flex-wrap min-h-screen items-center justify-center md:gap-2 dark:bg-gray-800">
       <ScoreWidget />
     </main>
   );
