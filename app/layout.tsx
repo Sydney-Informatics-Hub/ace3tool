b2fc1a1c8bba0508383ecd74d3d5e0b129d0ce66
marius-mather <marius.mather@sydney.edu.au>
2024-07-25 15:03:56 +1000

    Add navbar to default layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { ThemeModeScript } from "flowbite-react";
 import { ReactNode } from "react";
+import MainNavbar from "@/app/_components/MainNavbar";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -14,7 +15,10 @@ export default function RootLayout({ children }: { children: ReactNode }) {
       <head>
         <ThemeModeScript />
       </head>
-      <body>{children}</body>
+      <body>
+        <MainNavbar />
+        {children}
+      </body>
     </html>
   );
 }
