8041d13361e4df57d56342ef95369e4f83b7c442
marius-mather <marius.mather@sydney.edu.au>
2024-07-30 16:32:02 +1000

    Rework layout of score entry form

--- app/_forms/AceForm.tsx
+++ app/_forms/AceForm.tsx
@@ -14,27 +14,25 @@ export default function AceForm(props: AceFormProps) {
 
   const onSubmit: SubmitHandler<AceScaleScores> = (data) => console.log(data);
   return (
-    <Card className="max-w-xl grow">
-      <div className="flex flex-col justify-start">
-        <h2 className="text-2xl font-bold text-gray-900">
-          Enter ACE-III scale scores
-        </h2>
-        <FormProvider {...form}>
-          <form
-            className="flex flex-col max-w-lg gap-4 my-2"
-            onSubmit={handleSubmit(onSubmit)}
-          >
-            <ScoreInput label="Attention" name="attention" />
-            <ScoreInput label="Memory" name="memory" />
-            <ScoreInput label="Fluency" name="fluency" />
-            <ScoreInput label="Language" name="language" />
-            <ScoreInput label="Visuospatial" name="visuospatial" />
-          </form>
-        </FormProvider>
-        <Button type="submit" className="my-2 max-w-50">
-          Submit
-        </Button>
-      </div>
+    <Card className="max-w-xl" id="score-entry">
+      <h2 className="text-2xl font-bold text-gray-900">
+        Enter ACE-III scale scores
+      </h2>
+      <FormProvider {...form}>
+        <form
+          className="flex flex-col max-w-lg gap-4 my-2"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <ScoreInput label="Attention" name="attention" />
+          <ScoreInput label="Memory" name="memory" />
+          <ScoreInput label="Fluency" name="fluency" />
+          <ScoreInput label="Language" name="language" />
+          <ScoreInput label="Visuospatial" name="visuospatial" />
+        </form>
+      </FormProvider>
+      <Button type="submit" className="my-2 max-w-50">
+        Submit
+      </Button>
     </Card>
   );
 }
