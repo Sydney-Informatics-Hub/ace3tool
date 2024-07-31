ae9bc35a871180511877799834b6394fe29f5434
marius-mather <marius.mather@sydney.edu.au>
2024-07-31 15:38:07 +1000

    Try to add a legend to swarm plot

--- app/_components/SwarmPlotDisplay.tsx
+++ app/_components/SwarmPlotDisplay.tsx
@@ -8,16 +8,44 @@ type SwarmPlotDisplayProps = CardProps & {
   model: LogisticModel<keyof AceScaleScores>;
 };
 
+// TODO: creating the legend manually is not ideal
 export function SwarmPlotDisplay(props: SwarmPlotDisplayProps) {
   const { scores, model, ...card_props } = props;
   return (
     <Card {...card_props}>
       <div className="flex flex-col lg:flex-row lg:flex-wrap">
         <div id="swarm-intro" className="lg:basis-1/2 text-base max-w-prose">
-          The plot below shows the current scores in each subdomain as a line,
-          compared to scores from the sample of patients. Sample patients with
-          dementia are shown in white (⚪), and those without dementia in black
-          (⚫).
+          <h2 className="text-indigo-600 font-bold text-lg">
+            Subdomain scores
+          </h2>
+          <p>
+            This plot compares the current scores for each subdomain to those
+            from a sample of patients with dementia and healthy controls
+          </p>
+          <h3 className="text-lg text-indigo-800 font-bold mt-2">Legend</h3>
+          <ul className="my-1 space-y-2">
+            <li>
+              <span className="inline-flex items-center text-indigo-600 text-base p-1 border-2 me-2">
+                ●<hr className="w-4 h-1 bg-indigo-600" />●
+              </span>
+              Current score
+            </li>
+            <li>
+              <span className="text-red-600 font-bold p-1 border-2 me-2">
+                - - -
+              </span>
+              80% of dementia patients in the sample scored at or below this
+              level
+            </li>
+            <li>
+              <span className="text-xl text-gray-600 p-1 border-2 me-2">○</span>{" "}
+              Sample patient with dementia
+            </li>
+            <li>
+              <span className="text-xl text-black p-1 border-2 me-2">●</span>{" "}
+              Control patient (no dementia)
+            </li>
+          </ul>
         </div>
         <div className="lg:basis-1/2">
           <SwarmPlot scores={scores} model={model} />
