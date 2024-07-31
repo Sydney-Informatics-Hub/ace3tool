9b2e1006305ffde811e19e28ebe30f1ea081073b
marius-mather <marius.mather@sydney.edu.au>
2024-08-01 08:48:00 +1000

    Add bundle analyzer for checking site size

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
+import bundleAnalyzer from "@next/bundle-analyzer";
+
+const withBundleAnalyzer = bundleAnalyzer({enabled: process.env.ANALYZE === 'true'});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
 };
 
-export default nextConfig;
+export default withBundleAnalyzer(nextConfig);
