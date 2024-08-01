8c83545c022b4e14f809381133168ba4e932a7f7
marius-mather <marius.mather@sydney.edu.au>
2024-08-01 15:50:05 +1000

    Use flowbite List for legend

--- app/_components/SwarmPlotDisplay.tsx
+++ app/_components/SwarmPlotDisplay.tsx
@@ -1,4 +1,4 @@
-import { Card, CardProps } from "flowbite-react";
+import { Card, CardProps, List } from "flowbite-react";
 import { AceScaleScores } from "@/app/_forms/schemas/ace";
 import { LogisticModel } from "@/lib/logistic";
 import SwarmPlot from "@/app/_plots/SwarmPlot";
@@ -13,33 +13,33 @@ function SwarmPlotLegend(props: HTMLAttributes<HTMLDivElement>) {
   return (
     <div {...props}>
       <h2 className="text-indigo-600 font-bold text-lg">Subdomain scores</h2>
-      <p>
+      <p className="text-gray-800">
         This plot compares the current scores for each subdomain to those from a
         sample of patients with dementia and healthy controls
       </p>
       <h3 className="text-lg text-indigo-800 font-bold mt-2">Legend</h3>
-      <ul className="my-1 space-y-2">
-        <li>
+      <List unstyled className="my-1 space-y-3">
+        <List.Item>
           <span className="inline-flex items-center text-indigo-600 text-base p-1 border-2 me-2">
             ●<hr className="w-4 h-1 bg-indigo-600" />●
           </span>
           Current score
-        </li>
-        <li>
+        </List.Item>
+        <List.Item>
           <span className="text-red-600 font-bold p-1 border-2 me-2">
             - - -
           </span>
           80% of dementia patients in the sample scored at or below this level
-        </li>
-        <li>
+        </List.Item>
+        <List.Item>
           <span className="text-xl text-gray-600 p-1 border-2 me-2">○</span>{" "}
           Sample patient with dementia
-        </li>
-        <li>
+        </List.Item>
+        <List.Item>
           <span className="text-xl text-black p-1 border-2 me-2">●</span>{" "}
           Control patient (no dementia)
-        </li>
-      </ul>
+        </List.Item>
+      </List>
     </div>
   );
 }
