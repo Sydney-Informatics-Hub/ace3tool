38a70d44cc8d4658a74543f58e52f86e93a5b063
marius-mather <marius.mather@sydney.edu.au>
2024-08-01 15:11:07 +1000

    Remove unused theming

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,13 +4,7 @@ import flowbite from "flowbite-react/tailwind";
 const config: Config = {
   content: ["./app/**/*.{js,ts,jsx,tsx,mdx}", flowbite.content()],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+    extend: {},
   },
   plugins: [flowbite.plugin()],
 };
