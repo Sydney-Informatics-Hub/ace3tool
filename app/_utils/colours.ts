6fced55820be98d6b60c18702e42da115cb31fc8
marius-mather <marius.mather@sydney.edu.au>
2024-08-20 09:47:58 +1000

    Add some definitions for Tailwind colours so we can use them in code

---
+++ app/_utils/colours.ts
@@ -0,0 +1,10 @@
+/**
+ * Export a few tailwind colours for use in JavaScript
+ * There is a
+ */
+export const colours = {
+  orange500: "#f97316",
+  red600: "#dc2626",
+  red500: "#ef4444",
+  indigo600: "#4f46e5",
+};
