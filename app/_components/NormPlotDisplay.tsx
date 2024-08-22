d5af96d7f281836dd6c8a8f8f1c45c0241f7e660
marius-mather <marius.mather@sydney.edu.au>
2024-08-22 15:54:57 +1000

    Rework the legend for the NormPlot

--- app/_components/NormPlotDisplay.tsx
+++ app/_components/NormPlotDisplay.tsx
@@ -1,14 +1,86 @@
-import { Card, CardProps, List } from "flowbite-react";
+import { Card, CardProps, Table } from "flowbite-react";
 import { AceScaleScores } from "@/app/_forms/schemas/ace";
 import { LogisticModel } from "@/lib/logistic";
 import NormPlot from "@/app/_plots/NormPlot";
-import { HTMLAttributes } from "react";
+import { HTMLAttributes, ReactNode } from "react";
 import AreaIcon from "@/app/_components/AreaIcon";
 
-type SwarmPlotDisplayProps = CardProps & {
+type NormPlotDisplayProps = CardProps & {
   scores: Partial<AceScaleScores>;
   model: LogisticModel<keyof AceScaleScores>;
 };
+
+interface LegendTableItem {
+  icon: ReactNode;
+  label: string;
+}
+
+const legend_items = [
+  {
+    label: "Distribution in control participants",
+    icon: (
+      <AreaIcon
+        height={32}
+        width={32}
+        className="inline text-red-500 opacity-50 fill-current stroke-black stroke-2"
+      />
+    ),
+  },
+  {
+    icon: (
+      <AreaIcon
+        height={32}
+        width={32}
+        className="inline text-red-500 fill-current stroke-black stroke-2 rotate-180"
+      />
+    ),
+    label: "Distribution in dementia patients",
+  },
+  {
+    icon: (
+      <span className="inline-flex items-center justify-center text-indigo-600 text-base font-bold">
+        ○<hr className="w-3 h-1 bg-indigo-600" />○
+      </span>
+    ),
+    label: "Current score",
+  },
+  {
+    icon: <span className="text-orange-500 font-bold text-lg">······</span>,
+    label: "2 SDs below the mean for control patients",
+  },
+  {
+    icon: <span className="text-red-600 font-bold">- - - -</span>,
+    label: "100% specificity for detecting dementia in our sample",
+  },
+];
+
+/**
+ * Create the plot legend as table, where each row has an icon (can be any
+ * React element) and a label.
+ * @param props list of LegendTableItems, in order
+ */
+function LegendTable(props: { items: LegendTableItem[] }) {
+  return (
+    <Table className="table-fixed">
+      <Table.Body>
+        {props.items.map((item, index) => {
+          return (
+            <Table.Row key={index}>
+              <Table.Cell
+                className={
+                  "p-2 w-12 h-12 border-2 align-middle justify-center items-center text-center"
+                }
+              >
+                {item.icon}
+              </Table.Cell>
+              <Table.Cell className="p-2 h-12">{item.label}</Table.Cell>
+            </Table.Row>
+          );
+        })}
+      </Table.Body>
+    </Table>
+  );
+}
 
 function NormPlotLegend(props: HTMLAttributes<HTMLDivElement>) {
   return (
@@ -20,51 +92,18 @@ function NormPlotLegend(props: HTMLAttributes<HTMLDivElement>) {
         Scores are shown as a percentage of the maximum for each subdomain.
       </p>
       <h3 className="text-lg text-indigo-800 font-bold mt-2">Legend</h3>
-      <List unstyled className="my-1 space-y-3">
-        <List.Item>
-          <span className="inline-flex items-center text-indigo-600 text-base p-1 border-2 me-2">
-            ●<hr className="w-4 h-1 bg-indigo-600" />●
-          </span>
-          Current score
-        </List.Item>
-        <List.Item>
-          <span className="text-orange-500  p-1 border-2 me-2">·······</span>2
-          SDs below the mean for control patients
-        </List.Item>
-        <List.Item>
-          <span className="text-red-600 font-bold p-1 border-2 me-2">
-            - - - -
-          </span>
-          100% specificity for detecting dementia in our sample
-        </List.Item>
-        <List.Item>
-          <div className="inline p-1 py-2 border-2 me-2">
-            <AreaIcon
-              height={32}
-              width={32}
-              className="inline text-red-500 fill-current stroke-black stroke-2  -rotate-90 align-middle"
-            />
-          </div>
-          Distribution in patients with dementia
-        </List.Item>
-        <List.Item>
-          <span className="p-1 py-2 border-2 me-2">
-            <AreaIcon
-              height={32}
-              width={32}
-              className="inline text-red-500 opacity-50 fill-current stroke-black stroke-2 rotate-90"
-            />
-          </span>
-          Distribution in control patients
-        </List.Item>
-      </List>
+      <LegendTable items={legend_items} />
     </div>
   );
 }
 
-// TODO: creating the legend manually is not ideal, not sure how else to do it
-//   as observable plot doesn't seem to offer that level of customization
-export function NormPlotDisplay(props: SwarmPlotDisplayProps) {
+/**
+ * Show the NormPlot, which compares a patient's scores to the overall
+ * distribution, along with its legend, in a card.
+ * @param props
+ * @constructor
+ */
+export function NormPlotDisplay(props: NormPlotDisplayProps) {
   const { scores, model, ...card_props } = props;
   return (
     <Card {...card_props}>
