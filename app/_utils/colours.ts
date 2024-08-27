968e939a3ff5bfef1bee52b12cabd57e7faedd89
marius-mather <marius.mather@sydney.edu.au>
2024-08-27 11:06:11 +1000

    Add tableau colours for use in JS

--- app/_utils/colours.ts
+++ app/_utils/colours.ts
@@ -12,3 +12,16 @@ export const colours = {
   emerald500: "#10b981",
   emerald600: "#059669",
 };
+
+export const tableau10_colours = {
+  blue: "#5778a4",
+  orange: "#e49444",
+  red: "#d1615d",
+  teal: "#85b6b2",
+  green: "#6a9f58",
+  yellow: "#e7ca60",
+  purple: "#a87c9f",
+  pink: "#f1a2a9",
+  brown: "#967662",
+  grey: "#b8b0ac",
+};
