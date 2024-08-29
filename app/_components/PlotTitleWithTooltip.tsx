9f6b67db686854858069c24c751799630c6320f1
marius-mather <marius.mather@sydney.edu.au>
2024-08-29 15:00:57 +1000

    Add component for showing titles + tooltips for plots

---
+++ app/_components/PlotTitleWithTooltip.tsx
@@ -0,0 +1,30 @@
+import { ReactNode } from "react";
+import { Button, Popover } from "flowbite-react";
+import { QuestionMarkCircleIcon } from "@heroicons/react/24/solid";
+
+interface PlotTitleWithTooltipProps {
+  title: string;
+  tooltip_content: ReactNode;
+  button_content?: ReactNode;
+}
+
+export default function PlotTitleWithTooltip(props: PlotTitleWithTooltipProps) {
+  const { title, tooltip_content } = props;
+  return (
+    <div className="flex w-full items-center">
+      <h2 className="font-semibold text-indigo-600">{title}</h2>
+      <Popover
+        content={
+          <div className="w-80 p-2 justify-center">{tooltip_content}</div>
+        }
+        trigger="hover"
+      >
+        <Button className="ms-auto" color="blue" outline>
+          {props.button_content || (
+            <QuestionMarkCircleIcon className="size-5" />
+          )}
+        </Button>
+      </Popover>
+    </div>
+  );
+}
