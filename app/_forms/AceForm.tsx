48c6d27133c502eee3d29fa0ec850ebdd4d94fce
marius-mather <marius.mather@sydney.edu.au>
2024-09-02 11:45:27 +1000

    Add placeholder intro for the tool

--- app/_forms/AceForm.tsx
+++ app/_forms/AceForm.tsx
@@ -19,6 +19,22 @@ export default function AceForm(props: AceFormProps) {
   const onSubmit: SubmitHandler<AceScaleScores> = (data) => console.log(data);
   return (
     <Card {...card_props}>
+      <h1 className="text-xl font-bold text-indigo-600">ACE-III risk tool</h1>
+      <p className="text-base max-w-sm">
+        This tool is based on data from{" "}
+        <a
+          href="https://doi.org/10.1016/S1474-4422(23)00414-3"
+          className="text-blue-700"
+        >
+          Foxe (2024)
+        </a>
+        :{" "}
+        <i>
+          Demographic, clinical, biomarker, and neuropathological correlates of
+          posterior cortical atrophy: an international cohort study and
+          individual participant data meta-analysis
+        </i>
+      </p>
       <h2 className="text-xl font-bold text-indigo-600">
         Enter ACE-III subdomain scores
       </h2>
