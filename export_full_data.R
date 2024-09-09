c8915acbaac8bb988cad05b6051cfaf16aea7271
marius-mather <marius.mather@sydney.edu.au>
2024-09-09 13:04:03 +1000

    Add variables to the full data export

--- export_full_data.R
+++ export_full_data.R
@@ -2,6 +2,8 @@ library(tidyverse)
 library(jsonlite)
 library(readr)
 library(stringr)
+library(gtsummary)
+library(readxl)
 
 
 # Read in and clean data ######
@@ -24,9 +26,65 @@ data <- data %>%
                fct(levels=levels(dementia)),
            prob = 1 -  predict(model, type="response"))
 
+# Get some variables from the raw data set
+raw_data <- readxl::read_excel("data/PIPE-4796 Predictors of Dementia final data v1 7-8-2024.xlsx.xlsx")
+
+cleaned_scores <- data |> select(TotalScore, Attention:Visuospatial)
+raw_scores <- raw_data |> select(ends_with("(original)"))
+matching <- all.equal(cleaned_scores, raw_scores, check.names=FALSE)
+
+if (! matching) {
+    stop("Cleaned and raw data scores don't match, shouldn't be combined")
+}
+
+# Get variables from raw data
+# Apply any grouping here to make sure raw values don't make
+#  it into the exported dataset
+group_education <- function(x) {
+    case_when(
+        x < 10 ~ "9 or less",
+        x >= 10 & x < 13 ~ "10-12",
+        x >= 13 ~ "13+"
+    ) %>% as.factor()
+}
+
+group_goldman_score <- function(x) {
+    case_when(
+        is.na(x) ~ "Unknown",
+        between(x, 1, 2) ~ "1-2",
+        x >= 3 ~ "3+"
+    ) %>% as.factor()
+}
+
+data <- data |>
+    mutate(
+        VisitNumber = raw_data$Visit_order |>
+            case_match(
+                1 ~ "1",
+                c(2, 3) ~ "2 - 3",
+                4 ~ "4+",
+                .default = "4+"
+            ) |> as.factor(),
+        Education = raw_data$EducationYearsTotal %>%
+            group_education(),
+        GoldmanScore = raw_data$GoldmanScore %>%
+            group_goldman_score()
+    )
+
+# Check grouped variables
+data |>
+    select(VisitNumber, Education, GoldmanScore) |>
+    gtsummary::tbl_summary()
+
 # TODO: don't seem to have some variables: Goldman score, education
 exported_data <- data %>%
-    select(dementia, total=TotalScore, sex=Sex, learning_difficulties=LearningDifficulties, age_group=AgeGroup)
+    select(dementia, total=TotalScore, sex=Sex,
+           learning_difficulties=LearningDifficulties,
+           age_group=AgeGroup,
+           visit_number = VisitNumber,
+           education = Education,
+           goldman_score = GoldmanScore
+           )
 
 list(data = exported_data) |>
     jsonlite::write_json("app/_model/full_data_v1.json", dataframe = "rows")
