8fec3995a6bfcfdf512d10bea47594a27c21a66f
marius-mather <marius.mather@sydney.edu.au>
2024-09-10 10:26:52 +1000

    Improvements for help tooltips

--- app/_components/PlotTitleWithTooltip.tsx
+++ app/_components/PlotTitleWithTooltip.tsx
@@ -1,26 +1,47 @@
 import { ReactNode } from "react";
-import { Button, Popover } from "flowbite-react";
+import { Button, CustomFlowbiteTheme, Popover } from "flowbite-react";
 import { QuestionMarkCircleIcon } from "@heroicons/react/24/outline";
 import PlotTitle from "@/app/_components/PlotTitle";
+
+const customButtonTheme: CustomFlowbiteTheme["button"] = {
+  outline: {
+    color: {
+      indigo: "border-0",
+    },
+    on: "flex w-full justify-center bg-indigo-100 bg-opacity-80 text-indigo-900 transition-all duration-75 ease-in group-enabled:group-hover:bg-opacity-0 group-enabled:group-hover:text-inherit",
+  },
+};
 
 interface PlotTitleWithTooltipProps {
   title: string;
   tooltip_content: ReactNode;
   button_content?: ReactNode;
+  popover_id?: string;
+  padding?: number;
 }
 
 export default function PlotTitleWithTooltip(props: PlotTitleWithTooltipProps) {
   const { title, tooltip_content } = props;
+  const padding = props.padding !== undefined ? `p-${props.padding}` : "p-2";
   return (
     <div className="flex w-full items-center">
       <PlotTitle>{title}</PlotTitle>
       <Popover
         content={
-          <div className="w-96 p-2 justify-center">{tooltip_content}</div>
+          <div className={`w-96 ${padding} justify-center`}>
+            {tooltip_content}
+          </div>
         }
-        trigger="hover"
+        trigger="click"
+        id={props.popover_id || undefined}
       >
-        <Button className="ms-auto" size="sm" outline gradientMonochrome="info">
+        <Button
+          className="ms-auto"
+          size="sm"
+          outline
+          color="indigo"
+          theme={customButtonTheme}
+        >
           {props.button_content || (
             <QuestionMarkCircleIcon className="size-5" />
           )}
