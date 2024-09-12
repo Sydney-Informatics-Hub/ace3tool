83d1966f796eb49f3bbdde1ad424bcc167d5cc65
marius-mather <marius.mather@sydney.edu.au>
2024-09-12 15:22:12 +1000

    Initial cypress test

---
+++ cypress/e2e/accept_terms.cy.ts
@@ -0,0 +1,22 @@
+describe("Accepting terms and conditions", () => {
+  it("Modal is visible on first visit", () => {
+    cy.visit("http://localhost:3000/informatics/PIPE-5195-dementia-risk");
+    cy.contains("Terms and Conditions");
+    cy.get("button").contains("Accept");
+  });
+
+  it("Modal disappears after clicking accept, cookie is set", () => {
+    cy.visit("http://localhost:3000/informatics/PIPE-5195-dementia-risk");
+    cy.contains("Terms and Conditions");
+    cy.get("button").contains("Accept").click();
+    cy.contains("Terms and Conditions").should("not.exist");
+    cy.getCookie("accepted_terms").should("exist");
+    cy.getCookie("accepted_terms").should("have.property", "value", "true");
+  });
+
+  it("Modal should not be visible if cookie is already set", () => {
+    cy.setCookie("accepted_terms", "true");
+    cy.visit("http://localhost:3000/informatics/PIPE-5195-dementia-risk");
+    cy.contains("Terms and Conditions").should("not.exist");
+  });
+});
