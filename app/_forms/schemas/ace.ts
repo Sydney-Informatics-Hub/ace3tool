1fbd25342e2d867d67df33f8bd9013d8ee9f72e8
marius-mather <marius.mather@sydney.edu.au>
2024-09-13 11:22:24 +1000

    Rework score schemas: separate schema for inputs and final values

--- app/_forms/schemas/ace.ts
+++ app/_forms/schemas/ace.ts
@@ -1,4 +1,4 @@
-import { object, number, InferType, Message } from "yup";
+import { object, number, InferType, Message, string } from "yup";
 
 export const AceScaleInfo: Record<
   "attention" | "memory" | "fluency" | "language" | "visuospatial",
@@ -30,6 +30,54 @@ const max_score_message: Message = (data) => {
   return `The score for ${data.label} should be between 0 and ${data.max}`;
 };
 
+/**
+ * Schema for the form inputs of the ACE-III scales.
+ * Note we need two schemas: one for the form inputs, which need to accept
+ * null so they can be reset, and one for the final parsed values,
+ * which should all be numbers, and require all numbers entered to be
+ * considered valid
+ */
+export const AceScaleScoresInputSchema = object({
+  attention: number()
+    .label(AceScaleInfo.attention.label)
+    .integer()
+    .min(0)
+    .max(AceScaleInfo.attention.max, max_score_message)
+    .typeError("Please enter a number")
+    .nullable(),
+  memory: number()
+    .label(AceScaleInfo.memory.label)
+    .integer()
+    .min(0)
+    .max(AceScaleInfo.memory.max, max_score_message)
+    .typeError("Please enter a number")
+    .nullable(),
+  fluency: number()
+    .label(AceScaleInfo.fluency.label)
+    .integer()
+    .min(0)
+    .max(AceScaleInfo.fluency.max, max_score_message)
+    .typeError("Please enter a number")
+    .nullable(),
+  language: number()
+    .label(AceScaleInfo.language.label)
+    .integer()
+    .min(0)
+    .max(AceScaleInfo.language.max, max_score_message)
+    .typeError("Please enter a number")
+    .nullable(),
+  visuospatial: number()
+    .label(AceScaleInfo.visuospatial.label)
+    .integer()
+    .min(0)
+    .max(AceScaleInfo.visuospatial.max, max_score_message)
+    .typeError("Please enter a number")
+    .nullable(),
+});
+/**
+ * Schema for the final values of the ACE-III scale scores, with
+ * all values defined
+ */
 export const AceScaleScoresSchema = object({
   attention: number()
     .label(AceScaleInfo.attention.label)
@@ -68,6 +116,7 @@ export const AceScaleScoresSchema = object({
     .required(),
 });
 
+export type AceScaleScoresInput = InferType<typeof AceScaleScoresInputSchema>;
 export type AceScaleScores = InferType<typeof AceScaleScoresSchema>;
 
 export const AceScales: (keyof AceScaleScores)[] = [
@@ -77,3 +126,11 @@ export const AceScales: (keyof AceScaleScores)[] = [
   "language",
   "visuospatial",
 ];
+
+export const AceScaleScoresDefaultInputs = {
+  attention: null,
+  memory: null,
+  fluency: null,
+  language: null,
+  visuospatial: null,
+};
