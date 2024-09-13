220b7ef81e7b681647f2563e0e83951f12118026
marius-mather <marius.mather@sydney.edu.au>
2024-09-13 13:24:14 +1000

    Remove total score logistic model

--- app/_model/model.ts
+++ app/_model/model.ts
@@ -1,5 +1,4 @@
 import model_data from "@/app/_model/logistic_model_v1.json";
-import total_model_data from "@/app/_model/total_model_v1.json";
 import { Data, get_predictors, LogisticModel } from "@/lib/logistic";
 
 const logistic_model = new LogisticModel(model_data.coefs, {
@@ -8,11 +7,6 @@ const logistic_model = new LogisticModel(model_data.coefs, {
   center_predictors: true,
 });
 export default logistic_model;
-
-export const total_model = new LogisticModel(total_model_data.coefs, {
-  predictor_means: total_model_data.means,
-  center_predictors: true,
-});
 
 // TODO: should this be based on the dementia means instead?
 export interface DataSummary<predictors extends string> {
