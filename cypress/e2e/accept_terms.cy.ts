ac4752bc5176b6f702907a2776bdb7454229dea2
marius-mather <marius.mather@sydney.edu.au>
2024-09-17 15:57:10 +1000

    Update URLs in cypress tests

--- cypress/e2e/accept_terms.cy.ts
+++ cypress/e2e/accept_terms.cy.ts
@@ -1,18 +1,18 @@
 describe("Accepting terms and conditions", () => {
   it("Modal is visible on first visit", () => {
-    cy.visit("http://localhost:3000/informatics/PIPE-5195-dementia-risk");
+    cy.visit("/");
     cy.contains("Terms and Conditions");
     cy.get("button").contains("Accept");
   });
 
   it("Modal is visible on other pages", () => {
-    cy.visit("http://localhost:3000/informatics/PIPE-5195-dementia-risk/about");
+    cy.visit("/about");
     cy.contains("Terms and Conditions");
     cy.get("button").contains("Accept");
   });
 
   it("Modal disappears after clicking accept, cookie is set", () => {
-    cy.visit("http://localhost:3000/informatics/PIPE-5195-dementia-risk");
+    cy.visit("/");
     cy.contains("Terms and Conditions");
     cy.get("button").contains("Accept").click();
     cy.contains("Terms and Conditions").should("not.exist");
@@ -21,17 +21,17 @@ describe("Accepting terms and conditions", () => {
   });
 
   it("After clicking accept, modal doesn't appear on other pages", () => {
-    cy.visit("http://localhost:3000/informatics/PIPE-5195-dementia-risk");
+    cy.visit("/");
     cy.contains("Terms and Conditions");
     cy.get("button").contains("Accept").click();
     cy.contains("Terms and Conditions").should("not.exist");
-    cy.visit("http://localhost:3000/informatics/PIPE-5195-dementia-risk/about");
+    cy.visit("/about");
     cy.contains("Terms and Conditions").should("not.exist");
   });
 
   it("Modal should not be visible if cookie is already set", () => {
     cy.setCookie("accepted_terms", "true");
-    cy.visit("http://localhost:3000/informatics/PIPE-5195-dementia-risk");
+    cy.visit("/");
     cy.contains("Terms and Conditions").should("not.exist");
   });
 });
