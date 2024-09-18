3b91c92a357ee6aa3da7452965c5e8bcba6e5583
marius-mather <marius.mather@sydney.edu.au>
2024-09-18 10:35:52 +1000

    Try a separate Next config file for gh-pages

---
+++ next_ghpages.config.mjs
@@ -0,0 +1,19 @@
+import bundleAnalyzer from "@next/bundle-analyzer";
+import createMDX from '@next/mdx'
+
+const withBundleAnalyzer = bundleAnalyzer({enabled: process.env.ANALYZE === 'true'});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  basePath: "/ace3tool",
+  trailingSlash: true,
+  images: {unoptimized: true},
+  pageExtensions: ['md', 'mdx', 'ts', 'tsx'],
+};
+
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+})
+
+export default withMDX(withBundleAnalyzer(nextConfig));
