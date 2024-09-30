3133bd5b1b01a97d3de50270eb0a441cbff1064f
marius-mather <marius.mather@sydney.edu.au>
2024-09-30 12:00:05 +1000

    Add message for when terms are declined

--- app/_components/TermsModal.tsx
+++ app/_components/TermsModal.tsx
@@ -1,9 +1,10 @@
 "use client";
-import { Button, CustomFlowbiteTheme, Modal } from "flowbite-react";
+import { Alert, Button, CustomFlowbiteTheme, Modal } from "flowbite-react";
 import React, { useEffect } from "react";
 import { useCookies } from "react-cookie";
 import Image from "next/image";
 import frontier_logo from "@/app/_images/frontier_logo_small.png";
+import { InformationCircleIcon } from "@heroicons/react/24/solid";
 
 const customTheme: CustomFlowbiteTheme["modal"] = {
   header: {
@@ -20,6 +21,7 @@ export default function TermsModal() {
   const [cookies, setCookie, removeCookie] = useCookies(["accepted_terms"]);
   const accepted = cookies.accepted_terms === true;
   const [open, setOpen] = React.useState(false);
+  const [declined, setDeclined] = React.useState(false);
 
   useEffect(() => {
     setOpen(!accepted);
@@ -30,6 +32,9 @@ export default function TermsModal() {
     one_year.setFullYear(one_year.getFullYear() + 1);
     setCookie("accepted_terms", "true", { expires: one_year });
     setOpen(false);
+  };
+  const decline_terms = () => {
+    setDeclined(true);
   };
 
   return (
@@ -46,10 +51,10 @@ export default function TermsModal() {
       <Modal.Body>
         <div className="flex flex-col md:flex-row gap-2">
           <p className="grow inline">
-            This tool is designed exclusively for use by health professionals.
-            The ACE-III online dementia screening tool is not intended for
-            self-evaluation and must not be self-administered under any
-            circumstances.
+            This tool is designed exclusively for use by qualified health
+            professionals. The ACE-III online dementia screening tool is not
+            intended for self-evaluation and must not be self-administered under
+            any circumstances.
           </p>
           <Image
             className="inline"
@@ -61,12 +66,28 @@ export default function TermsModal() {
             priority
           />
         </div>
+        {declined && (
+          <Alert className="mt-2" color="failure" icon={InformationCircleIcon}>
+            Thank you for your response. This tool is intended for use by
+            qualified health professionals only. If you are seeking further
+            information or resources on dementia, please visit{" "}
+            <a
+              className="text-red-900 underline"
+              href="https://frontierftd.org/"
+            >
+              our website
+            </a>
+            .
+          </Alert>
+        )}
       </Modal.Body>
       <Modal.Footer>
         <Button color="success" onClick={() => accept_terms()}>
           Accept
         </Button>
-        <Button color="gray">Decline</Button>
+        <Button color="gray" onClick={decline_terms}>
+          Decline
+        </Button>
       </Modal.Footer>
     </Modal>
   );
